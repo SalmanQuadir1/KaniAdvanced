75dcafa2bacf47755a75b68dd394509fdd4b9973
UMER MUKHTAR <shah.umer8493@gmail.com>
2025-01-01 09:44:56 +0530

    view

--- src/components/Order/ViewOrder.jsx
+++ src/components/Order/ViewOrder.jsx
@@ -252,7 +252,7 @@ const ViewOrder = () => {
 
                 <td className="px-5 py-5 border-b border-gray-200 text-sm">
                     <p className="flex text-gray-900 whitespace-no-wrap">
-                        <FiEdit size={17} className='text-teal-500 hover:text-teal-700 mx-2' onClick={(e) => handleUpdate(e, item)} title='Edit Product' />  |
+                        <FiEdit size={17} className='text-teal-500 hover:text-teal-700 mx-2' onClick={()=>navigate(`/Order/updateorder/${item?.id}`)} title='Edit Product' />  |
                         <FiTrash2 size={17} className='text-red-500 hover:text-red-700 mx-2' onClick={(e) => handleDelete(e, item?.id)} title='Delete Product' />
                     </p>
                 </td>
