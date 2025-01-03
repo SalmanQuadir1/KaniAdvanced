915a299a621ccaf68501774129cf2869b11fd62e
UMER MUKHTAR <shah.umer8493@gmail.com>
2025-01-03 09:43:18 +0530

    index

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -289,8 +289,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                           !open && 'hidden'
                         }`}
                       >
+                        
                         <ul className="mt-4 mb-5.5 flex flex-col gap-2.5 pl-6">
-                        <li>
+                        {/* <li>
                             <NavLink
                               to="/inventory/addProductInventory"
                               className={({ isActive }) =>
@@ -300,7 +301,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                             >
                               Add Inventory
                             </NavLink>
-                          </li>
+                          </li> */}
+
                         
                           <li>
                             <NavLink
