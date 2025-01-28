48463eb55df49312dda68e55a192f1a74c186aeb
UMER MUKHTAR <shah.umer8493@gmail.com>
2025-01-28 10:57:55 +0530

    add

--- src/components/Products/AddProduct.jsx
+++ src/components/Products/AddProduct.jsx
@@ -1088,7 +1088,7 @@ const AddProduct = () => {
                                                         )}
 
 
-                                                        <div className="flex-1 min-w-[300px]">
+                                                        {/* <div className="flex-1 min-w-[300px]">
                                                             <label className="mb-2.5 block text-black dark:text-white"> MRP</label>
                                                             <Field
                                                                 name='mrp'
@@ -1096,7 +1096,7 @@ const AddProduct = () => {
                                                                 placeholder="Enter MRP"
                                                                 className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-Field dark:text-white dark:focus:border-primary"
                                                             />
-                                                        </div>
+                                                        </div> */}
                                                     </div>
 
 
@@ -1285,7 +1285,7 @@ const AddProduct = () => {
                                                                 className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-Field dark:text-white dark:focus:border-primary"
                                                             />
                                                         </div>
-                                                        <div className="flex-2 min-w-[250px]">
+                                                        {/* <div className="flex-2 min-w-[250px]">
                                                             <label className="mb-2.5 block text-black dark:text-white"> MRP</label>
                                                             <Field
                                                                 name='mrp'
@@ -1293,7 +1293,7 @@ const AddProduct = () => {
                                                                 placeholder="Enter MRP"
                                                                 className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-Field dark:text-white dark:focus:border-primary"
                                                             />
-                                                        </div>
+                                                        </div> */}
                                                         <div className="flex-2 min-w-[250px]">
                                                             <label className="mb-2.5 block text-black dark:text-white"> Dyeing Cost </label>
                                                             <Field
@@ -1501,7 +1501,7 @@ const AddProduct = () => {
                                                                 className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-Field dark:text-white dark:focus:border-primary"
                                                             />
                                                         </div>
-                                                        <div className="flex-2 min-w-[250px]">
+                                                        {/* <div className="flex-2 min-w-[250px]">
                                                             <label className="mb-2.5 block text-black dark:text-white"> MRP</label>
                                                             <Field
                                                                 name='mrp'
@@ -1509,7 +1509,7 @@ const AddProduct = () => {
                                                                 placeholder="Enter MRP"
                                                                 className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-Field dark:text-white dark:focus:border-primary"
                                                             />
-                                                        </div>
+                                                        </div> */}
                                                         <div className="flex-2 min-w-[250px]">
                                                             <label className="mb-2.5 block text-black dark:text-white"> Dyeing Cost </label>
                                                             <Field
@@ -1717,7 +1717,7 @@ const AddProduct = () => {
 
 
                                                     <div className="mb-4.5 flex flex-wrap gap-6">
-                                                        <div className="flex-2 min-w-[250px]">
+                                                        {/* <div className="flex-2 min-w-[250px]">
                                                             <label className="mb-2.5 block text-black dark:text-white"> MRP</label>
                                                             <Field
                                                                 name='mrp'
@@ -1725,7 +1725,7 @@ const AddProduct = () => {
                                                                 placeholder="Enter MRP"
                                                                 className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-Field dark:text-white dark:focus:border-primary"
                                                             />
-                                                        </div>
+                                                        </div> */}
 
                                                         <div className="flex-2 min-w-[250px]">
                                                             <label className="mb-2.5 block text-black dark:text-white"> Dyeing Cost </label>
@@ -1894,7 +1894,7 @@ const AddProduct = () => {
                                                                 className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-Field dark:text-white dark:focus:border-primary"
                                                             />
                                                         </div>
-                                                        <div className="flex-2 min-w-[250px]">
+                                                        {/* <div className="flex-2 min-w-[250px]">
                                                             <label className="mb-2.5 block text-black dark:text-white"> MRP</label>
                                                             <Field
                                                                 name='mrp'
@@ -1902,7 +1902,7 @@ const AddProduct = () => {
                                                                 placeholder="Enter MRP"
                                                                 className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-Field dark:text-white dark:focus:border-primary"
                                                             />
-                                                        </div>
+                                                        </div> */}
                                                         <div className="flex-2 min-w-[250px]">
                                                             <label className="mb-2.5 block text-black dark:text-white"> Dyeing Cost </label>
                                                             <Field
