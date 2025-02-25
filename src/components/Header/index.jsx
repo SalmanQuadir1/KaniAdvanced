f4ec06186b0d2df59cf59f8ffd9172aef81ea70d
UMER MUKHTAR <shah.umer8493@gmail.com>
2025-02-25 15:17:11 +0530

    index

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import DropdownMessage from './DropdownMessage';
 import DropdownNotification from './DropdownNotification';
 import DropdownUser from './DropdownUser';
 import LogoIcon from '../../images/logo/logo-icon.svg';
 import DarkModeSwitcher from './DarkModeSwitcher';
-
+import { FaHome } from "react-icons/fa";
 const Header = (props) => {
+
+  const navigate = useNavigate()
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
@@ -101,6 +103,10 @@ const Header = (props) => {
         <div className="flex items-center gap-3 2xsm:gap-7">
           <ul className="flex items-center gap-2 2xsm:gap-4">
             <DarkModeSwitcher />
+          </ul>
+          <ul>
+
+          <FaHome className='cursor-pointer' size={30} onClick={() => navigate("/home")}/>
           </ul>
 
           {/* <!-- User Area --> */}
