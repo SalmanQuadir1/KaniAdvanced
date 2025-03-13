862d76449a58800513b164125a76d7cc2f0d86e7
UMER MUKHTAR <shah.umer8493@gmail.com>
2025-03-13 12:10:24 +0530

    add

--- src/components/Products/AddProduct.jsx
+++ src/components/Products/AddProduct.jsx
@@ -452,7 +452,7 @@ const AddProduct = () => {
                                     {/* <!-- Contact Form --> */}
                                     <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
                                         <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
-                                            <h3 className="font-medium text-slate-500 text-center text-xl dark:text-white">
+                                            <h3 className="font-semibold text-slate-500 text-center text-2xl dark:text-white">
                                                 Add Product
                                             </h3>
                                         </div>
