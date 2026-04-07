bd172c256bec096c1af7967faa62f5bb355c1765
umer <shah.umer8493@gmail.com>
2026-04-07 16:03:02 +0530

    dfvd

--- src/components/Accounts/Config/DayBook.jsx
+++ src/components/Accounts/Config/DayBook.jsx
@@ -189,7 +189,7 @@ const ViewDayBook = () => {
                             '0.00'
                         )}
                     </td> */}
-                    <td className="px-5 py-5 border-b border-gray-200 text-sm">
+                    {/* <td className="px-5 py-5 border-b border-gray-200 text-sm">
                         <div className="flex">
                             <FiEdit
                                 size={17}
@@ -204,7 +204,7 @@ const ViewDayBook = () => {
                                 title="Delete Entry"
                             />
                         </div>
-                    </td>
+                    </td> */}
                 </tr>
             );
         });
@@ -323,7 +323,7 @@ const ViewDayBook = () => {
                                         <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Credit</th>
                                         <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Amount</th> */}
                                         <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Amount</th>
-                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Action</th>
+                                        {/* <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Action</th> */}
                                     </tr>
                                 </thead>
                                 <tbody>
